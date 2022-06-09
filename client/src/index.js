9e6a0d4bce0c01e2116de31c0330d2b3db12de08
Anand kumar satapthy <anandkumarsatapathy@gmail.com>
2022-06-09 16:38:36 +0530

    "Redux Toolkit"

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,16 @@ import react from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose } from "redux";
+import { configureStore, applyMiddleware, compose } from "@reduxjs/toolkit";
 import thunk from "redux-thunk";
 import { reducers } from './reducers';
 
+const store = configureStore(reducers, compose(applyMiddleware(thunk)));
 
 
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
